0c9a4edd00221fad877f2c5e3e67aa74e251df3e
Kris Jenkins <krisajenkins@gmail.com>
2016-04-12 16:42:35 +0100

    Correcting the default template.
    I'd left some cruft in there.

--- templates/default/src/Types.elm
+++ templates/default/src/Types.elm
@@ -3,22 +3,9 @@ module Types (..) where
 import Dict exposing (..)
 
 
-type alias Position =
-  ( Int, Int )
-
-
-type alias World a =
-  Dict Position a
-
-
-type Cell
-  = Block
-  | Path
-
-
 type Action
   = NoOp
 
 
 type alias Model =
-  { world : World Cell }
+  {}
